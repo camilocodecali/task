5b72aa45b8de426e43a45aec478413807a7b58ec
Camilocodecali <camilorodriguezco7@gmail.com>
2024-11-02 03:07:25 -0500

    Se creo el componente de Editar y se trae el dato del proyecto a editar por id

--- src/api/ProjectAPI.ts
+++ src/api/ProjectAPI.ts
@@ -1,6 +1,6 @@
 
 import api from "@/lib/axios";
-import { dashboardProjectSchema, ProjectFormData } from "../types";
+import { dashboardProjectSchema, Project, ProjectFormData } from "../types";
 import { isAxiosError } from "axios";
 
 export async function createProject(formData: ProjectFormData) {
@@ -24,6 +24,22 @@ export async function getProjects() {
         if(response.success){
             return response.data
         }
+
+    } catch (error) {
+        if(isAxiosError(error) && error.response){
+            throw new Error(error.response.data.error);
+        }
+
+    }
+
+}
+
+export async function getProjectById(id: Project['_id']) {
+    try {
+        const {data} = await api(`/projects/${id}`)
+        return data
+        console.log(data);
+        
 
     } catch (error) {
         if(isAxiosError(error) && error.response){
