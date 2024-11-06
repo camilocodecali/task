8e00e069c55fcf23ee0db403c6d1aea9d57d577b
Camilocodecali <camilorodriguezco7@gmail.com>
2024-11-05 22:23:31 -0500

    Invalidar cache en la edicion de proyectos, eliminar proyectos y creamos ruta de detalle proyecto

--- src/components/projects/EditProjectForm.tsx
+++ src/components/projects/EditProjectForm.tsx
@@ -2,7 +2,7 @@ import { useForm } from 'react-hook-form'
 import ProjectForm from './ProjectForm'
 import { Link } from 'react-router-dom'
 import { Project, ProjectFormData } from '@/types/index'
-import { useMutation } from '@tanstack/react-query'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
 import { updateProject } from '@/api/ProjectAPI'
 import { toast } from 'react-toastify'
 import { useNavigate } from 'react-router-dom'
@@ -22,12 +22,15 @@ const {register, handleSubmit, formState: {errors}} = useForm({defaultValues:{
 
 }})
 
+const queryClient = useQueryClient()
 const { mutate } = useMutation({
   mutationFn: updateProject,
   onError: (error) => {
     toast.error(error.message)
   },
   onSuccess: (data) => {
+    queryClient.invalidateQueries({queryKey:['projects']})
+    queryClient.invalidateQueries({queryKey:["editProject", projectId]})
     toast.success(data)
     navigate('/')
   }
