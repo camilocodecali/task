04e20225e7f321f4f07c5918a5b3515d6d382f1f
Camilocodecali <camilorodriguezco7@gmail.com>
2024-11-05 22:41:47 -0500

    Se creo pagina de detalle de proyecto y se creo modal para nueva tarea de ese proyecto

--- src/views/projects/ProjectDetailsView.tsx
+++ src/views/projects/ProjectDetailsView.tsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import { useParams, useNavigate, Navigate } from "react-router-dom"
+import { useQuery } from "@tanstack/react-query";
+import { getProjectById } from "@/api/ProjectAPI";
+import AddTaskModal from "@/components/tasks/AddTaskModal";
 
 export default function ProjectDetailsView() {
-  return (
-    <div>
-      ProjectDetail
-    </div>
+
+    const navigate = useNavigate()
+
+    const params= useParams()
+    const projectId = params.projectId!
+    const { data, isLoading, isError } = useQuery({
+        queryKey: ["editProject", projectId],
+        queryFn: ()=> getProjectById(projectId),
+        retry: false
+      });
+
+      if (isLoading) return "Cargando...";
+      if(isError) return <Navigate to='/404' />
+    
+  if(data)return (
+    <>
+    <h1 className="text-5xl font-black">{data.projectName}</h1>
+    <p className="text-2xl font-light text-gray-500 mt-5">{data.description}</p>
+
+    <nav className="my-5 flex gap-3">
+        <button 
+            className="bg-purple-400 hover:bg-purple-500 px-10 py-3 text-white text-xl font-bold cursor-pointer transition-colors"
+            type="button"
+            onClick={()=> navigate('?newTask=true')}
+        >
+            Agregar Tarea
+        </button>
+    </nav>
+    <AddTaskModal/>
+    </>
   )
 }
