0ff2387a0adcb598453fbcca672358787edf096c
Camilocodecali <camilorodriguezco7@gmail.com>
2024-11-20 22:27:27 -0500

    Se redirecciona en la pagina de login, se restringe accesos a las diferentes paginas si no esta autenticado y se devuelve a login

--- src/api/ProjectAPI.ts
+++ src/api/ProjectAPI.ts
@@ -7,17 +7,18 @@ export async function createProject(formData: ProjectFormData) {
     try {
         const {data} = await api.post('/projects', formData)
         return data
-        
+
     } catch (error) {
         if(isAxiosError(error) && error.response){
             throw new Error(error.response.data.error);
         }
 
     }
-    
+
 }
 
 export async function getProjects() {
+
     try {
         const {data} = await api('/projects')
         const response = dashboardProjectSchema.safeParse(data)
